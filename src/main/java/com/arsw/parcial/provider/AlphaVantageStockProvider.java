a1d4cbe7e78e0ac66ebaa5f0a59f6426c32e680a
Diego Cardenas <diegosp778@gmail.com>
2025-09-20 09:23:41 -0500

    feat: implemnent logic and controllers for stock

--- src/main/java/com/arsw/parcial/provider/AlphaVantageStockProvider.java
+++ src/main/java/com/arsw/parcial/provider/AlphaVantageStockProvider.java
@@ -1,5 +1,48 @@
 package com.arsw.parcial.provider;
 
-public class AlphaVantageStockProvider {
-    
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.arsw.parcial.models.StockDataDTO;
+
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class AlphaVantageStockProvider implements StockProvider {
+
+    @Value("${alphavantage.api.key}")
+    private String apiKey;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private String buildUrl(String function, String symbol) {
+        return "https://www.alphavantage.co/query?function=" + function +
+                "&symbol=" + symbol +
+                "&apikey=" + apiKey +
+                "&outputsize=full";
+    }
+
+    @Override
+    public StockDataDTO getIntraday(String symbol) {
+        String url = buildUrl("TIME_SERIES_INTRADAY", symbol) + "&interval=5min";
+        return restTemplate.getForObject(url, StockDataDTO.class);
+    }
+
+    @Override
+    public StockDataDTO getDaily(String symbol) {
+        String url = buildUrl("TIME_SERIES_DAILY", symbol);
+        return restTemplate.getForObject(url, StockDataDTO.class);
+    }
+
+    @Override
+    public StockDataDTO getWeekly(String symbol) {
+        String url = buildUrl("TIME_SERIES_WEEKLY", symbol);
+        return restTemplate.getForObject(url, StockDataDTO.class);
+    }
+
+    @Override
+    public StockDataDTO getMonthly(String symbol) {
+        String url = buildUrl("TIME_SERIES_MONTHLY", symbol);
+        return restTemplate.getForObject(url, StockDataDTO.class);
+    }
 }
